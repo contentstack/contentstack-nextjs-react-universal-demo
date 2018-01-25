be2df9e14c2a15a1beec8922f1c5c0242bbabd4f
Pradip_Kolhe <pradip.kolhe@raweng.com>
2018-01-25 18:34:21 +0530

    updated in config file

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 module.exports = {
   contentstack: {
-    api_key: 'Please Enter your stack api_key',
-    access_token: 'Please Eneter your stack access_token',
+    api_key: 'Please Enter your Stack api_key',
+    access_token: 'Please Eneter your Stack access_token',
     environment: 'Environment'
   }
 }
