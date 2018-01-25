f2aafb90ff868d5150b23bfc83b50c39850c32d5
Rohit Mishra <Rohit>
2018-01-25 19:04:41 +0530

    updated in development and package file

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 module.exports = {
   contentstack: {
     api_key: 'Please Enter your Stack api_key',
-    access_token: 'Please Eneter your Stack access_token',
+    access_token: 'Please Enter your Stack access_token',
     environment: 'Environment'
   }
 }
