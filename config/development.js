6f1e928d2ad7af8907d2a7b0750cc7d883ea0dbf
Rohit Mishra <Rohit>
2018-01-25 18:21:07 +0530

    updated in config files

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 module.exports = {
   contentstack: {
-    api_key: 'blt50806c1dbb5972f5',
-    access_token: 'blt2e51b1c00a53540f',
-    environment: 'development'
+    api_key: 'Please Enter your stack api_key',
+    access_token: 'Please Eneter your stack access_token',
+    environment: 'Environment'
   }
 }
